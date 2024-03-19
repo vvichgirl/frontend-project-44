92dcca1a3a81727b3733de085cb6e189710ebe36
Viktoriia Zubkova <vichgirl@gmail.com>
2024-03-19 18:07:57 +0300

    install readline-sync, user's greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log("Welcome to the Brain Games!");
+import getName from '../src/cli.js';
+const greeting = getName();
+console.log(greeting);
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    const userName = readlineSync.question('May I have your name?');
+    const result = 'Hello, ' + userName + '!';
+
+    return result;
+}
